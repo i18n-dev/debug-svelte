6e4297943fb792cac407918c94801931d653eeaf
i18n.host <i18n.host@gmail.com>
2025-06-06 13:37:06 +0800

    🔥

---
+++ Test.svelte
@@ -0,0 +1,18 @@
+<svelte:options customElement={{ tag: "i-test", shadow: "none" }} />
+
+<script>
+  import { onMount } from "svelte";
+  onMount(() => {
+    console.log(456);
+  });
+</script>
+
+<template>
+  <p>123</p>
+</template>
+
+<style>
+  p {
+    font-size: 20px;
+  }
+</style>
