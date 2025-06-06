577eb1ead358eb05ae08d1fefcb39642eb13e2c1
i18n.host <i18n.host@gmail.com>
2025-06-06 13:56:18 +0800

    ♨

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ cd $DIR
 set -ex
 
 cd src
-bun x vite build --watch
+bun x vite build
---
+++ src/Index.svelte
@@ -0,0 +1,16 @@
+<script lang="coffee">
+> svelte > onMount
+
+onMount =>
+  console.log '>'
+  return
+
+</script>
+
+<template lang="pug">
+
+</template>
+
+<style lang="stylus">
+
+</style>
